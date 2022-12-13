8fa1ca0232ce7358761c259d3d9d0cb17a354db3
omar khaled <omar_khaled-66@hotmail.com>
2022-12-14 01:33:47 +0200

    cartt

---
+++ Pages/User/Cart/Index.cshtml.cs
@@ -0,0 +1,66 @@
+using MedicineProject.DataAccess.Repository.IRepository;
+using MedicineProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace MedicineProject.Pages.User.Cart
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+        public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+        public double CartTotal { get; set; }
+        private readonly IUnitOfWork _unitOfWork;
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            CartTotal= 0;
+        }
+
+        public void OnGet()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if(claim != null) 
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(filter: u => u.ApplicationUserId == claim.Value,
+                    includeProperties:"MenuItem,MenuItem.MedicineType,MenuItem.Category");
+                
+                foreach(var cartItem in ShoppingCartList)
+                {
+                    CartTotal += (cartItem.MenuItem.Price * cartItem.Count);
+                }
+            }
+        }
+        public IActionResult OnPostPlus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+            return RedirectToPage("/User/Cart/Index");
+        }
+        public IActionResult OnPostMinus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+            if(cart.Count == 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+                _unitOfWork.Save();
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+            }
+            return RedirectToPage("/User/Cart/Index");
+        }
+        public IActionResult OnPostRemove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+            return RedirectToPage("/User/Cart/Index");
+        }
+        
+    }
+}
--- Pages/User/Home/Details.cshtml.cs
+++ Pages/User/Home/Details.cshtml.cs
@@ -3,6 +3,7 @@ using MedicineProject.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NuGet.Common;
 using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 
@@ -34,8 +35,16 @@ namespace MedicineProject.Pages.User.Home
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.ShoppingCart.Add(ShoppingCart);
-                _unitOfWork.Save();
+                ShoppingCart shoppingCartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(filter: u => u.ApplicationUserId == ShoppingCart.ApplicationUserId && u.MenuItemId == ShoppingCart.MenuItemId);
+                if(shoppingCartFromDb == null) 
+                {
+                    _unitOfWork.ShoppingCart.Add(ShoppingCart);
+                    _unitOfWork.Save();
+                }
+                else
+                {
+                    _unitOfWork.ShoppingCart.IncrementCount(shoppingCartFromDb, ShoppingCart.Count);
+                }
                 return RedirectToPage("Index");
             }
             return Page();
