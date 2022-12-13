355b9df1e7ad427cd71f22402e45d1f0fe44a808
omar khaled <omar_khaled-66@hotmail.com>
2022-12-13 23:47:06 +0200

    additemtocart

--- Pages/User/Home/Details.cshtml.cs
+++ Pages/User/Home/Details.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 
 namespace MedicineProject.Pages.User.Home
 {
@@ -19,10 +20,25 @@ namespace MedicineProject.Pages.User.Home
         public ShoppingCart ShoppingCart { get; set; }
         public void OnGet(int id)
         {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
             ShoppingCart = new()
             {
-                MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,MedicineType")
+                ApplicationUserId = claim.Value,
+                MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,MedicineType"),
+                MenuItemId = id
             };
-        } 
+        }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.ShoppingCart.Add(ShoppingCart);
+                _unitOfWork.Save();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
     }
 }
